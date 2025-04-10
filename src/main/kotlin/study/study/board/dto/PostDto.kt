adbb379c2cb8932a9de51c707625220e59ba2f4d
minjun <kmjsuwon3136@suwon.ac.kr>
2025-04-10 18:39:06 +0900

    board

--- src/main/kotlin/study/study/board/dto/PostDto.kt
+++
@@ -1,7 +0,0 @@
-package study.study.board.dto
-
-data class PostDto(
-    val title: String,
-    val content: String,
-    val author: String
-)
--- src/main/kotlin/study/study/board/model/Post.kt
+++
@@ -1,9 +0,0 @@
-package study.study.board.model
-
-data class Post(
-    val id: Long,
-    val title: String,
-    val content: String,
-    val author: String,
-    val createdAt: String
-)
